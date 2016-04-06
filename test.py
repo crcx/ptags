e34f6ac72a929f711f8b9cd55441ca1b1824b3d2
Charles Childers <crc@forthworks.com>
2016-04-06 11:21:14 +0000

    begin work on merging test scanner into tag generation tool

--- test.py
+++ test.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# generate a ctags compatible tags file for parable
+#
+# constraints:
+#
+# - code can be in either a *.p or *.md file
+#    - in *.p, everything is code
+#    - in *.md, code either has four spaces at the start of the line or
+#      is fenced with four backticks (````)
+# - syntax for naming a function can be complicated
+#    - basic form:
+#      [ ... ] 'name' :
+#    - alternate form:
+#      'name' [ ... ] .
+#    - variable forms:
+#      'name' var
+#
+#      ... 'name' var!
+#
+#      [ 'name' 'name2' 'name3' ... ] ::
+#    - functions can span multiple source lines:
+#      'hello' [ "-s" \
+#          'hello world!' ] .
+# - tag file formats
+#   - classic ctags (for vim):
+#      tag  filename  line#
+#   - textmate requires:
+#      tag  filename  /^search_string$/;"  function  line:line#
+#   - replace the separators with a single tab
+
+import argparse
 import fnmatch
 import os
 
+
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 # form 0: not a definition
 # form 1: [ ... ] 'name' :
@@ -37,6 +70,18 @@ def extract_tags(src, form):
                 tags.append(t[1:-1])
     return tags
 
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+def tag(tag, textmate=False):
+    name = tag[0]
+    file = os.getcwd() + '/' + tag[1][2:]
+    line = tag[2]
+    if textmate:
+        return name + '\t' + file + '\t' + '/^\'' + name + '\'$/;"\tfunction\tline:' + str(line)
+    else:
+        return name + '\t' + file + '\t' + str(line)
+
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 def get_tags_for(pat, textmate=False):
     tags = []
@@ -62,9 +107,24 @@ def get_tags_for(pat, textmate=False):
 
 def write_tags(tagfile, textmate=False):
     tags = get_tags_for('*.p')
-    for l in tags:
-        print(l)
+    tags = tags + get_tags_for('*.md')
+    with open(tagfile, 'w') as f:
+        for l in sorted(tags):
+            f.write(tag(l, textmate) + '\n')
+    return len(tags)
 
 
 if __name__ == '__main__':
-    write_tags('tags')
+    parser = argparse.ArgumentParser(description='ctags for parable')
+    parser.add_argument('--ctags', help='generate vim compatible tags', action="store_true")
+    parser.add_argument('--tmtags', help='generate textmate compatible tags', action="store_true")
+    args = vars(parser.parse_args())
+
+    if args['ctags']:
+        write_tags('tags')
+
+    if args['tmtags']:
+        write_tags('tmtags', textmate=True)
+
+    if not args['ctags'] and not args['tmtags']:
+        print('Specify tags format!')
